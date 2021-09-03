050dea86caab208a5aa46b669f974716de6e4203
Carl Avidano <cavidano@gmail.com>
2021-09-03 10:00:22 -0400

    before updating state

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import BlogList from './BlogList';
 
 function Home() {
 
@@ -10,19 +11,8 @@ function Home() {
 
     return (
         <div className="home">
-            {blogs.map((blog) => (
-
-                <div
-                    className="blog-preview"
-                    key={blog.id}
-                >
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-
-                </div>
-
-            ))}
-            
+            <BlogList blogs={blogs} title="All Blogs" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" />
         </div>
     )
 }
