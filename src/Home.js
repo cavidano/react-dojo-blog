52b09cdfad3bb8ed649a190569a4dd8ce830ba69
Carl Avidano <cavidano@gmail.com>
2021-09-03 09:43:39 -0400

    before creating blog list component

--- src/Home.js
+++ src/Home.js
@@ -2,24 +2,26 @@ import React, { useState } from 'react';
 
 function Home() {
 
-    const [name, setName] = useState('Mario');
-    const [age, setAge] = useState(25);
-
-    const handleClick = () => {
-        setName('Luigi');
-        setAge(40);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ])
 
     return (
         <div className="home">
+            {blogs.map((blog) => (
 
-            <h2>Homepage</h2>
+                <div
+                    className="blog-preview"
+                    key={blog.id}
+                >
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
 
-            <p>{name} is {age} years old</p>
+                </div>
 
-            <button onClick={handleClick}>
-                Click me
-            </button>
+            ))}
             
         </div>
     )
