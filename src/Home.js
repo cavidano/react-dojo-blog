38918735f06881f55f55ee69554653044c6bd056
Carl Avidano <cavidano@gmail.com>
2021-09-03 10:46:07 -0400

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 function Home() {
@@ -9,11 +9,21 @@ function Home() {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const [name, setName] = useState('Mario');
+
     const handleDelete = (id) => {
 
         const newBlogs = blogs.filter((blog) => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+
+    // Use Effect
+
+    useEffect(() => {
+        console.log('Use Effect Ran');
+        console.log(name);
+    }, [name]);
 
     return (
         <div className="home">
@@ -21,7 +31,10 @@ function Home() {
                 blogs={blogs}
                 title="All Blogs"
                 handleDelete={handleDelete}
-                 />
+            />
+            <button onClick={() => setName('Luigi') }>Change Name</button>
+            <p>{name}</p>
+
         </div>
     )
 }
